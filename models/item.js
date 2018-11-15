186e70f4b433244d43f660a6821664a990587d7a
bliliana <lilianablg@gmail.com>
2018-11-15 11:04:06 +0200

    Added Item schema

---
+++ models/item.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var itemSchema = Schema({
+    _id: Schema.Types.ObjectId,
+    title: String,
+    completed: Boolean
+});
+
+module.exports = mongoose.model('Item', itemSchema)
