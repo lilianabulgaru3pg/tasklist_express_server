2e8bc5bc50c988a4035742ccdf8741ae4e8a854a
bliliana <lilianablg@gmail.com>
2018-11-22 17:05:09 +0200

    Renamed user-tasks to tasks

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const session = require('express-session')
 const FileStore = require('session-file-store')(session);
 const multer = require('multer');
 var upload = multer();
+var cors = require('cors')
 
 var db = require('./utils/database');
 var userRouter = require('./routes/userRouter.js');
@@ -33,24 +34,26 @@ app.use(passport.session());
 
 app.use(express.static('public'));
 app.use(express.static('../TodoListApp/dist/'));
+app.use(cors());
 // app.use(express.static('../TodoListApp/src/'));
 // app.use(express.static('../TodoListApp/src/main.css'));
 // app.use(express.static('../TodoListApp/src/index.js'));
 
-app.post('/user-tasks', upload.none(), userRouter);
-app.get('/user-tasks', isLoggedIn, userRouter);
-app.get('/user-tasks/:taskId', userRouter);
-app.get('/user-tasks/:taskId/items', userRouter);
-app.post('/user-tasks/add-task', userRouter);
-app.post('/user-tasks/:taskId/add-item', userRouter);
-app.put('/user-tasks/:itemId/update', userRouter);
+app.post('/tasks', upload.none(), userRouter);
+app.get('/tasks', isLoggedIn, userRouter);
 app.get('/logout', userRouter);
+app.get('/tasks/:taskId', userRouter);
+app.get('/tasks/:taskId/items', userRouter);
+app.post('/tasks/add-task', userRouter);
+app.post('/tasks/:taskId/add-item', userRouter);
+app.put('/tasks/:itemId/update', userRouter);
+app.get('/tasks/search', userRouter);
 
 function isLoggedIn(req, res, next) {
     console.log('isLoggedIn', req.isAuthenticated());
     if (req.isAuthenticated()) {
         next();
-        //res.redirect('/user-tasks/:taskId');
+        //res.redirect('/tasks/:taskId');
     }
     res.redirect('/');
 }
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -4,13 +4,15 @@ var router = express.Router();
 
 var user_controller = require('../controllers/userController');
 
-router.post('/user-tasks', user_controller.user_tasks);
-router.post('/user-tasks/add-task', user_controller.create_task);
-router.get('/user-tasks', user_controller.user_authrequired);
-router.get('/user-tasks/:taskId', user_controller.user_taskId);
-router.get('/user-tasks/:taskId/items', user_controller.get_items);
-router.post('/user-tasks/:taskId/add-item', user_controller.create_item);
-router.put('/user-tasks/:itemId/update', user_controller.update_item);
+router.post('/tasks', user_controller.user_tasks);
+router.post('/tasks/add-task', user_controller.create_task);
+router.get('/tasks', user_controller.user_authrequired);
+router.get('/tasks/search', user_controller.item_search);
+router.get('/tasks/:taskId', user_controller.user_taskId);
+router.get('/tasks/:taskId/items', user_controller.get_items);
+router.post('/tasks/:taskId/add-item', user_controller.create_item);
+router.put('/tasks/:itemId/update', user_controller.update_item);
 router.get('/logout', user_controller.user_logout);
+
 
 module.exports = router;
